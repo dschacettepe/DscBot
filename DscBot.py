ee52609257c82d7b141ede4671d0b74712e60f7d
utkubora <56562267+utkubora@users.noreply.github.com>
2022-02-12 01:52:55 +0300

    Update DscBot.py v1.8.1
    -typo fixed

--- DscBot.py
+++ DscBot.py
@@ -210,7 +210,7 @@ async def help(ctx):
     embed.add_field(name="!!help", value="komutların işlevini gösterir.", inline=False)
     #embed.add_field(name="!!kalan_sure", value="Hackatonun bitimine kalan süreyi verir.", inline=False)
     #embed.add_field(name="!!yasakli_kelime_ekle", value="Yasaklı kelime listesine yeni kelimeler eklemenizi sağlar.", inline=False)
-    embed.add_field(name="!!kalan_sure", value="Hackaton'un bitimine kalan süreyi verir.", inline=False)
+    embed.add_field(name="!!kalan_sure", value="Hackathon'un bitimine kalan süreyi verir.", inline=False)
     embed.add_field(name="!!mentor_describe <mentor-isim>", value="İsmi girilen mentor veya mentorlerin bilgilerini gösterir.", inline=False)
     embed.add_field(name="!!mentor_destek", value="Mentorlere yardım istediğinize dair bir bildirim gider.", inline=False)
     embed.add_field(name="!!teknik_destek", value="Tekniik destek ekibine yardım istediğinize dair bir bildirim gider.", inline=False)
